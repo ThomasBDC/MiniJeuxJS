83a81e94bc9b0cba3eddc32fca75d25c47857625
Thomas BUREAU DU COLOMBIER <sio.bureauducolombier@gmail.com>
2022-11-08 12:01:54 +0100

    Finish pendu game

--- Pendu/pendu.js
+++ Pendu/pendu.js
@@ -2,11 +2,13 @@
 TODO : 
     X Générer un mot aléatoire
     X Afficher le mot en masqué _ _ _ _ _ _ _
-    O Pouvoir proposer des lettres
-    O Afficher les lettres trouvées
-    O Gérer un nombre d'erreur max
+    X Pouvoir proposer des lettres
+    X Afficher les lettres trouvées
+    X Gérer un nombre d'erreur max
+    O Gérer la victoire    
     O Afficher des lettres visibles (en fonction de la difficulté)
 */
+import { Confetti } from "../lib/confetti.js";
 
 const allWords = ["ministre", "congolais", "constitution",
  "corompre", "petrole", "dictateur", "sapeur",
@@ -14,6 +16,12 @@ const allWords = ["ministre", "congolais", "constitution",
 const buttonPlay = document.getElementById("beginGame");
 const wordToFindDiv = document.getElementById("wordToFindDiv");
 const KeyBoardDiv= document.getElementById("KeyBoard");
+const cptErreurDiv= document.getElementById("cptErreur");
+const imgPendu = document.getElementById("imagePendu");
+let wordToFind;
+let wordToFindArray;
+let cptErreur =0;
+let cptLettreTrouvees =0;
 
 buttonPlay.addEventListener("click", function(){
     initGame();
@@ -21,14 +29,20 @@ buttonPlay.addEventListener("click", function(){
 
 
 function initGame(){
-    //Générer un mot au hasard
+    //Générer un mot au hasard*
+    Confetti.stopAnimationConfeti();
+    cptErreur = 0;
+    imgPendu.className = '';
+    imgPendu.classList.add("etat"+cptErreur);   
+    cptLettreTrouvees =0;
     wordToFindDiv.innerHTML = '';
-    let wordToFind = generateWord();
-    let wordToFindArray = Array.from(wordToFind);
+    wordToFind = generateWord();
+    wordToFindArray = Array.from(wordToFind);
     
     let table = document.createElement("table");
     
     let line = document.createElement("tr");
+    line.id="LineOfWord";
     wordToFindArray.forEach(letter => {
         //Créer un TD (case du tableau) par lettre
         let td = document.createElement("td");
@@ -51,6 +65,48 @@ function generateKeyBoard(){
         lettreDiv.innerHTML = letter;
         lettreDiv.classList.add("letterKeyBoard");
         KeyBoardDiv.appendChild(lettreDiv);
+
+        lettreDiv.addEventListener("click", () => {
+            if(checkLetterInWord(letter)){
+                //Afficher la lettre dans le mot masqué
+                let lineWord = document.getElementById("LineOfWord");
+                let allTdOfWord = lineWord.children;
+                Array.from(allTdOfWord).forEach(td => {
+                    if(td.dataset.letter == letter){
+                        td.innerHTML = letter;
+                        cptLettreTrouvees ++;
+                    }
+                });
+
+                if(cptLettreTrouvees == wordToFindArray.length){
+                    KeyBoardDiv.innerHTML = '';
+                    cptErreurDiv.innerHTML = 'Gagné, avec '+cptErreur+' erreur(s)';
+                    Confetti.launchAnimationConfeti();
+                    setTimeout(() =>{
+                        Confetti.stopAnimationConfeti();
+                    }, 5000);
+                }
+            }
+            else{
+                //Incrémenter le compteur d'erreur
+                cptErreur++;
+                cptErreurDiv.innerHTML = cptErreur;
+                imgPendu.className = '';
+                imgPendu.classList.add("etat"+cptErreur);
+                if(cptErreur >= 4){
+                    //On a perdu
+                    cptErreurDiv.innerHTML = "Perdu, vous avez fait plus de 4 erreurs.";
+                    let lineWord = document.getElementById("LineOfWord");
+                    let allTdOfWord = lineWord.children;
+                    Array.from(allTdOfWord).forEach(td => {
+                        td.innerHTML = td.dataset.letter;
+                    });
+                    KeyBoardDiv.innerHTML = '';
+                }
+            }
+
+            lettreDiv.style.visibility = "hidden";
+        })
     });
 }
 
@@ -80,4 +136,16 @@ function generateWord(){
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
+}
+
+//Retourne true si la lettre est présente dans le mot
+//Retourne false si la lettre est absente du mot
+function checkLetterInWord(letter){
+    let findLetter = false;
+    wordToFindArray.forEach(letterOfWord => {
+        if(letter == letterOfWord){
+            findLetter =  true;
+        } 
+    });
+    return findLetter;
 }
