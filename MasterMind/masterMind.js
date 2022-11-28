eb4d5027d176e18907e71d72b5613e76aa30bb87
Thomas BUREAU DU COLOMBIER <sio.bureauducolombier@gmail.com>
2022-11-28 12:04:00 +0100

    generate colors and selects

--- MasterMind/masterMind.js
+++ MasterMind/masterMind.js
@@ -2,9 +2,9 @@
  * 
  * TODO LIST
  * 
- * Générer la combinaison secrète (4 couleurs)
- * Pouvoir proposer une combinaison
- * Gérer début et fin de partie
+ * OK Générer la combinaison secrète (4 couleurs)
+ *    Pouvoir proposer une combinaison
+ *    Gérer début et fin de partie
  * 
  * 
  * Ajouter d'autres éléments si on a le temps
@@ -12,5 +12,43 @@
 import { Utils } from "../lib/Utils/utils.js";
 
 const colors = ["red", "blue", "yellow", "pink"];
+let colorTabToFind = null;
+const nbColorToFind = 4;
 
-console.log(colors[Utils.getRandomInt(4)])
+document.getElementById("startButton").addEventListener("click", ()=> {
+    launchGame();
+});
+
+function launchGame(){
+    setAleaColorTab();
+    document.getElementById("allSelect").innerHTML = "";
+    for (let index = 0; index < nbColorToFind; index++) {
+        generateSelect("allSelect");
+    }
+}
+
+function generateSelect(idCible){
+    let mySelect = document.createElement("select");
+    colors.forEach(color => {
+        let colorOption = document.createElement("option");
+        colorOption.innerHTML = color;
+        colorOption.value = color;
+        colorOption.style.backgroundColor = color;
+        mySelect.appendChild(colorOption);
+    });
+
+    document.getElementById(idCible).appendChild(mySelect);
+}
+
+function setAleaColorTab(size = 4){
+    colorTabToFind = [];
+    for (let index = 0; index < size; index++) {
+        colorTabToFind.push(getAleaColor());
+    }
+}
+
+function getAleaColor(){
+    let aleaIndex = Utils.getRandomInt(colors.length);
+    let aleaColor = colors[aleaIndex];
+    return aleaColor;
+}
