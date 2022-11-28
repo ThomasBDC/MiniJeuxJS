f8e4642b13b7d765f205858dd5e0df5b2ba2cdf2
Thomas BUREAU DU COLOMBIER <sio.bureauducolombier@gmail.com>
2022-11-28 11:37:48 +0100

    Add Utils class + refacto

---
+++ MasterMind/masterMind.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ * TODO LIST
+ * 
+ * Générer la combinaison secrète (4 couleurs)
+ * Pouvoir proposer une combinaison
+ * Gérer début et fin de partie
+ * 
+ * 
+ * Ajouter d'autres éléments si on a le temps
+ */
+import { Utils } from "../lib/Utils/utils.js";
+
+const colors = ["red", "blue", "yellow", "pink"];
+
+console.log(colors[Utils.getRandomInt(4)])
